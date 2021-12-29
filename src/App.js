aa3ee0daf32ceb84c736997190fccd85f327946d
Josh Bryson <josh@bryson.me.uk>
2021-12-29 12:36:50 +0000

    Initial Suspense fallback setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
-import LazyLoader from './components/LazyLoader/LazyLoader';
+import ColourSpinner from './components/LazyLoader/ColourSpinner';
 import { ReactComponent as CaretIcon } from './components/Header/Icons/caret-down-outline.svg';
 import './App.scss';
 
@@ -9,17 +9,17 @@ import TextDisplay from './pages/TextDisplay';
 import DrawShape from './pages/DrawShape';
 import LanguageOptions from './pages/LanguageOptions';
 
-// const TextDisplayPage = React.lazy(() => import('./pages/TextDisplay'));
-
 function App() {
   return (
     <Router>
       <Header navIcon={<CaretIcon />} title={'React Voice Recognition'} />
-      <Routes>
-        <Route exact path="/" element={<TextDisplay />} />
-        <Route exact path="/shape-addition" element={<DrawShape />} />
-        <Route exact path="/language" element={<LanguageOptions />} />
-      </Routes>
+      <React.Suspense fallback={<ColourSpinner />}>
+        <Routes>
+          <Route exact path="/" element={<TextDisplay />} />
+          <Route exact path="/shape-addition" element={<DrawShape />} />
+          <Route exact path="/language" element={<LanguageOptions />} />
+        </Routes>
+      </React.Suspense>
     </Router>
   )
 }
